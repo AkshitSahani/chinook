fcdd9ac6210bdf08227446876035f44c121386c5
Akshit Sahani <akshit_sahani@hotmail.com>
2017-02-01 17:16:19 -0500

    finished assignment with stretch

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -6,29 +6,32 @@
 
 # 1a) Find the genre with the name "Hip Hop/Rap".
 
-
+Genre.where(name: "Hip Hop/Rap")
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
 
-
+Track.where(genre_id: '17').count
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
 
-
+Track.all.sum(:milliseconds)
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
 
-
+a = MediaType.where(name: "MPEG audio file")
+a.maximum(:unit_price)
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
 
-
+b = a.maximum(:unit_price)
+Track.where(unit_price: b)
 
 # 4) Find the 2 oldest artists.
 
-
+Artist.all.order(:created_at).limit(2)
 
 ### Stretch Exercise (Active Record Query Interface)
 
 
 # 1) Find all the albums whose titles start with B.
+Album.where("title LIKE?","B%")
--- sql_assignment.sql
+++ sql_assignment.sql
@@ -6,18 +6,22 @@
 
 -- 1) Find the albums recorded by the artist Queen.
 
-
+SELECT * FROM albums WHERE artist_id = '51';
+-- 51 is the artist_id for 'Queen' which i found by doing SELECT artist_id FROM artists WHERE name='Queen'
 
 -- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
 
-
+SELECT COUNT(*) FROM media_types WHERE name='Protected MPEG-4 video file';
 
 -- 3) Find the least expensive track that has the genre "Electronica/Dance".
 
-
+SELECT name, min(unit_price) AS price FROM tracks WHERE genre_id='15' GROUP BY name;
+-- 15 is the genre_id for 'Electronica/Dance' which i found by doing SELECT genre_id FROM genres WHERE name='Electronica/Dance'
 
 -- 4) Find the all the artists whose names start with A.
 
-
+SELECT * FROM artists WHERE name LIKE 'A%';
 
 -- 5) Find all the tracks that belong to playlist 1.
+
+SELECT id, name FROM tracks where id IN (SELECT track_id FROM playlists_tracks WHERE playlist_id='1');
